4102ed886bb4dba9ffe68f460d9d5adb20f64fce
Sumesh-42 <sumesh.pandita@gmail.com>
2026-02-07 14:36:21 +0530

    Access Control System Script

---
+++ python-logic-assignment/Access_Control_System.py
@@ -0,0 +1,10 @@
+age = 20  
+has_id = True
+# Access Control System
+# Determine access based on age and ID
+if age >= 18 and has_id == True:
+    print("Access granted.")
+else:
+    print("Access denied.")
+
+
