855eb53c72b8776d0faced7c0ec6cb5f0c6a048e
Sumesh-42 <sumesh.pandita@gmail.com>
2026-02-08 21:14:56 +0530

    Add Two Numbers

---
+++ python_functions_assignment/Add_Two_Numbers.py
@@ -0,0 +1,7 @@
+def add_two_numbers(num1, num2):
+    return num1 + num2
+# Call the function and print the result
+number1 = 10
+number2 = 5
+result = add_two_numbers(number1, number2)
+print(f"Sum is: {result}")
