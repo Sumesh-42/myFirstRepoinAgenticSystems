1a2ce65e7d6a1b63fdb246b246c62b0198324f26
Sumesh-42 <sumesh.pandita@gmail.com>
2026-02-08 21:16:36 +0530

    Even Odd Script

---
+++ python_functions_assignment/Even_or_Odd_Checker.py
@@ -0,0 +1,9 @@
+def even_or_odd_checker(number):
+    if number % 2 == 0:
+        return "Number is Even"
+    else:
+        return "Number is Odd"
+# Example usage
+num = 7
+result = even_or_odd_checker(num)
+print(result)
